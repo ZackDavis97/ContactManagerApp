8881ce30f5dc24adec3902b381423842523985cf
Zack Davis <jantrezac95@gmail.com>
2022-04-27 14:44:02 -0500

    minor javascript and css

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import PeopleList from './components/PeopleList';
+import AddPersonForm from './components/AddPersonForm';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import './style.css';
