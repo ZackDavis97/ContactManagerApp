cb10a70318c4f27caf3eaac973fa6c5eaaff4740
Zack Davis <jantrezac95@gmail.com>
2022-04-27 15:27:38 -0500

    additionns to website

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ import AddPersonForm from './components/AddPersonForm';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import './style.css';
+
+const initialState = {
+    contacts: ["Bob Sagot", "Hillary Duff", "Luna"]
+    };
+//reducer
+function reducer(state = initialState, action) {
+    switch(action.type){
+        case 'ADD_PERSON':
+            return {...state,
+            contacts: [...state.contacts, action.data]}
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+ReactDOM.render(
+    <Provider store={store}>
+        <AddPersonForm />
+        <PeopleList />
+    </Provider>,
+    document.getElementById('root')
+);
